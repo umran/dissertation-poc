955b39cc14ed5cc6000029cd027bb50f771573cd
Umran Hussain <1547890+umran@users.noreply.github.com>
2025-08-15 11:09:29 +0100

    created xl config

--- src/bac/tests/test_td3_pendulum.py
+++ src/bac/tests/test_td3_pendulum.py
@@ -12,5 +12,6 @@ if torch.cuda.is_available():
 
 test = Manifest("./out/test", device=DEVICE)
 
+test.xl("td3_pendulum", Pendulum, TD3, 20_000)
 test.baselines("td3_pendulum", Pendulum, TD3, 20_000)
 test.ablation("td3_pendulum", Pendulum, TD3, 20_000)
