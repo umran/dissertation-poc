0102085a8454951314d42354cd6ee85ec9a1889f
Umran Hussain <1547890+umran@users.noreply.github.com>
2025-08-21 14:58:21 +0100

    implemented walker environment

--- src/bac/environments/__init__.py
+++ src/bac/environments/__init__.py
@@ -4,5 +4,6 @@ from .inverted_pendulum import InvertedPendulum
 from .inverted_double_pendulum import InvertedDoublePendulum
 from .hopper import Hopper
 from .mountain_car import MountainCar
+from .walker_2d import Walker2D
 
-__all__ = ["Environment", "Pendulum", "InvertedPendulum", "InvertedDoublePendulum", "Hopper", "MountainCar"]
+__all__ = ["Environment", "Pendulum", "InvertedPendulum", "InvertedDoublePendulum", "Hopper", "MountainCar", "Walker2D"]
---
+++ src/bac/environments/walker_2d.py
@@ -0,0 +1,41 @@
+import torch
+import gymnasium as gym
+from typing import Tuple, Any
+
+from .environment import Environment
+
+class Walker2D(Environment):
+    def __init__(self, render_mode: str = "rgb_array", device: torch.device = torch.device("cpu")):
+        self.env = gym.make("Walker2d-v5", render_mode=render_mode)
+        self.device = device
+
+        self.env.reset(seed=torch.randint(0, 1 << 32, (1,), dtype=torch.int64).item())
+
+    def state_shape(self) -> Tuple[int, ...]:
+        return (17, )
+
+    def action_shape(self) -> Tuple[int, ...]:
+        return (6, )
+    
+    def action_min(self) -> torch.Tensor:
+        return torch.tensor([-1.0, -1.0, -1.0, -1.0, -1.0, -1.0], dtype=torch.float32, device=self.device)
+
+    def action_max(self) -> torch.Tensor:
+        return torch.tensor([1.0, 1.0, 1.0, 1.0, 1.0, 1.0], dtype=torch.float32, device=self.device)
+
+    def reset(self) -> torch.Tensor:
+        observation, _ = self.env.reset()
+        observation = torch.tensor(observation, dtype=torch.float32, device=self.device)
+
+        return observation
+
+    def step(self, action: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, Any]:
+        action = action.detach().cpu().numpy()
+        observation, reward, terminated, truncated, info = self.env.step(action)
+
+        observation = torch.tensor(observation, dtype=torch.float32, device=self.device)
+        reward = torch.tensor(reward, dtype=torch.float32, device=self.device)
+        terminated = torch.tensor(terminated, dtype=torch.bool, device=self.device)
+        truncated = torch.tensor(truncated, dtype=torch.bool, device=self.device)
+
+        return (observation, reward, terminated, truncated, info)
